5739241fdbec9e374ab495a8c9faf97558cf4277
Manav1011 <manavshah1011.ms@gmail.com>
2024-01-25 00:01:44 +0530

    trying fixes

--- src/app/contact-us/get-a-quote/page.js
+++ src/app/contact-us/get-a-quote/page.js
@@ -1,5 +1,5 @@
-import GetAQUote from '../../components/ContactUs/GetAQUote'
-import CompanyInformation from '../../components/ContactUs/CompanyInformation'
+import GetAQUote from '../../../components/ContactUs/GetAQUote'
+import CompanyInformation from '../../../components/ContactUs/CompanyInformation'
 function page() {
   return (
     <section className="h-full mb-20 flex lg:mx-10 lg:flex-row flex-col" style={{fontFamily:'oswald'}}>
