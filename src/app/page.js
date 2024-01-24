f884e3919401c53654fbe5e77ecbd77fe8293674
Manav1011 <manavshah1011.ms@gmail.com>
2024-01-24 23:08:08 +0530

    trying to find the issue

--- src/app/page.js
+++ src/app/page.js
@@ -9,22 +9,22 @@ export default async function Home() {
     "bg-gradient-to-r from-violet-500 to-fuchsia-500",
     "bg-gradient-to-r from-purple-500 to-pink-500"
   ]
-  const ServiceListing = dynamic(() => import("./components/home/ServiceListing"), {
-    suspense: true,
-    ssr:false,
-    loading: () => <ServiceSkeleton/>
-  });
-  const TeamSection = dynamic(() => import("./components/home/TeamSection"), {
-    suspense: true,
-    ssr:false,
-    loading: () => <ServiceSkeleton/>
-  });
+  // const ServiceListing = dynamic(() => import("./components/home/ServiceListing"), {
+  //   suspense: true,
+  //   ssr:false,
+  //   loading: () => <ServiceSkeleton/>
+  // });
+  // const TeamSection = dynamic(() => import("./components/home/TeamSection"), {
+  //   suspense: true,
+  //   ssr:false,
+  //   loading: () => <ServiceSkeleton/>
+  // });
   const choosed_grad_index = Math.floor(Math.random() * gradient_arr.length)
   return (
     <>      
       <MainGraphic grad={gradient_arr[choosed_grad_index]}/>                    
-      <ServiceListing/>                    
-      <TeamSection/>      
+      {/* <ServiceListing/>                     */}
+      {/* <TeamSection/>       */}
     </>
   )
 }
